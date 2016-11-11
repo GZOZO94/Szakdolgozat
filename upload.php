3cc78264ab16f475d6c57ed7ccf551c1484bcf70
GZOZO94 <gaszol11.c@gmail.com>
2016-11-11 11:23:35 +0100

    'try'

--- upload.php
+++ upload.php
@@ -18,27 +18,27 @@ if(isset($_FILES['image']['tmp_name']) && $_POST["txt"]!=NULL)
 			$file=md5(uniqid()).".".$file_extension;
 			$targetPath = "Uploads/".$file;
 			move_uploaded_file($sourcePath,$targetPath);
-			$textfile=md5(uniqid()).".txt";
-			$myfile=fopen('Texts/'.$textfile,'w')  or die("Nem lehet megnyitni a fájlt!");
-			fwrite($myfile, $text);
-			fclose($myfile);
-			$res=pg_query($con,"select * from ref");
-			while($result=pg_fetch_array($res))
-			{
-				if($ref_Id==$result["ref_id"])
-				{
-					if($result['long_text']!=NULL)
-					{
-						$filename=$result["long_text"];
-						unlink('Texts/'.$filename);
-					}
-				}
-			}
 			$query=sprintf("insert into pictures(pic_name,references_ref_id) values('%s',%d)",pg_escape_string($file),$ref_Id);
 			pg_query($con,$query);
-			$querys=sprintf("update ref set long_text='%s' where ref_id='%d'",pg_escape_string($textfile),$ref_Id);
-			pg_query($con,$querys);
 		}
+		$textfile=md5(uniqid()).".txt";
+		$myfile=fopen('Texts/'.$textfile,'w')  or die("Nem lehet megnyitni a fájlt!");
+		fwrite($myfile, $text);
+		fclose($myfile);
+		$res=pg_query($con,"select * from ref");
+		while($result=pg_fetch_array($res))
+		{
+			if($ref_Id==$result["ref_id"])
+			{
+				if($result['long_text']!=NULL)
+				{
+					$filename=$result["long_text"];
+					unlink('Texts/'.$filename);
+				}
+			}
+		}
+		$querys=sprintf("update ref set long_text='%s' where ref_id='%d'",pg_escape_string($textfile),$ref_Id);
+			pg_query($con,$querys);
 		echo "Sikeres kép, és szöveg feltöltés!";
 	}
 else if(!isset($_FILES['image']['tmp_name']) && $_POST["txt"]!=NULL)
