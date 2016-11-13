499b4f2fce3eec2d1c8cf5b15f96a0a0950ce035
GZOZO94 <gaszol11.c@gmail.com>
2016-11-14 00:55:22 +0100

    'frissites'

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 	}
 	else
 	$eye=0;
-	ini_set('upload_max_filesize', '10M');
+	 ini_set('post_max_size', '100M'); ini_set('upload_max_filesize', '100M'); ini_set('memory_limit', '1000M'); ini_set('max_execution_time', '1920'); 
 		echo phpinfo();
 ?>
 <!DOCTYPE HTML>
